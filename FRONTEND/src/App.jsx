4839079eaa08d00f319da006d5bfce32a819be35
Heshan-404 <heshantharushka2002@gmail.com>
2024-05-12 00:39:43 +0530

    added routing

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingUI from "./components/Homepage/LandingUI";
 function App() {
   return (
-    <>
-      <LandingUI></LandingUI>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingUI />} />
+        <Route path="/admin" element={<LandingUI />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
