c9b4c1aa8d8baffcecd84a70b7a04e4d18c31322
Heshan-404 <heshantharushka2002@gmail.com>
2024-05-17 22:21:08 +0530

    changed database port

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3306;
 
 // Database Configuration (These will be set as environment variables in Vercel)
 const dbConfig = {
