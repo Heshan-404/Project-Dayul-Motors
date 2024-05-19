95a51fd8651d3c23eae8468e286a90801c883bf0
Heshan-404 <163637857+Heshan-404@users.noreply.github.com>
2024-05-19 11:53:32 +0530

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -13,8 +13,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASSWORD, 
+    user: "pubg200212111@gmail.com", 
+    pass: "mbmj gbew qeoz nflk", 
   },
 });
 
