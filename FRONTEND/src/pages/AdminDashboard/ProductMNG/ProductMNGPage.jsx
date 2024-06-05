cdd33c40e6f254742115e30f3390e57ba3aae372
BimsaraIUHS <bimsara4iuhs@gmail.com>
2024-06-05 10:33:26 +0530

    just added add item to run the interface

--- FRONTEND/src/pages/AdminDashboard/ProductMNG/ProductMNGPage.jsx
+++ FRONTEND/src/pages/AdminDashboard/ProductMNG/ProductMNGPage.jsx
@@ -1,4 +1,5 @@
 import AllProducts from "../../../components/AdminDashboard/ProductMNG/AllProducts";
+import Additem from "../../../components/AdminDashboard/ProductMNG/Additem";
 
 export default function ProductMNGPage() {
   return (
